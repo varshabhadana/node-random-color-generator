1b10860e4072bdabd59dc3b9cf6454167fa49309
Varsha <varshabhadana93@gmail.com>
2022-09-26 13:00:55 +0200

    Remove un-used code

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-import { argv } from 'node:process';
+/* import { argv } from 'node:process'; */
 import chalk from 'chalk';
 import randomcolor from 'randomcolor';
 
-const hexCode = randomcolor.randomColor({
-  luminosity: argv[3],
-  hue: argv[2],
-});
+const hexCode = randomcolor.randomColor();
 
 const block = `###############################
 ###############################
